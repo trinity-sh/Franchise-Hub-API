73dc923c3b81501e45e3f98af6a7a9af0295a27a
jay-io <jay.dnb@outlook.in>
2022-05-28 00:41:09 +0530

    Remove non-requested features, add auth route for admin login

--- authServer.js
+++ authServer.js
@@ -1,69 +1,36 @@
 const express = require('express');
 const jwt  = require('jsonwebtoken');
 const bcrypt  = require('bcrypt');
+const mongoose = require('mongoose');
+
+const admcred = require('./schemas/admin-login-cred');
+const admtoken = require('./schemas/admin-access-token');
 
 require('dotenv').config();
-require('./mdb').connectdb();
 
-const AdminCred = require('./schemas/admin-login-cred');
-const InvestorCred = require('./schemas/investor-login-cred');
-const InvestorData = require('./schemas/investor-data');
-const FranchisorCred = require('./schemas/franchisor-login-cred');
-const FranchisorData = require('./schemas/franchisor-data');
-
+// init. express
 const app = express();
+require('./mdb').connectdb();
 
 // register middleware
 app.use(express.json());
 
-// user registration
-app.post('/api/v1/investor/register', async (req, res) => {
+// admin login
+app.post('/api/v1/admin/login', async (req, res) => {
         try {
-                const icred = await InvestorCred.create({
-                        _id: req.body.email,
-                        password: req.body.password
-                });
-        
-                const idata = await InvestorData.create({
-                        _id: req.body.email,
-                        name: req.body.name,
-                        mobile_no: req.body.mobile_no,
-                        dob: req.body.dob,
-                        street_address: req.body.street_address,
-                        city: req.body.city,
-                        state: req.body.state,
-                        country: req.body.country,
-                        pincode: req.body.pincode,
-                        industry_type: req.body.industry_type,
-                        income_range: req.body.income_range,
-                        investment_range: req.body.investment_range,
-                        avail_capital: req.body.avail_capital,
-                        need_for_loan: req.body.need_for_loan,
-                        investment_when: req.body.investment_when,
-                        educ_qual: req.body.educ_qual,
-                        occupation: req.body.occupation,
-                        prior_xp: req.body.prior_xp,
-                        looking_for: req.body.looking_for,
-                        lfb_in_city: req.body.lfb_in_city,
-                        lfb_in_state: req.body.lfb_in_state,
-                        own_property: req.body.own_property
-                });
+                const adminUser = await admcred.findById(req.body.username);
+                if (adminUser == null)
+                        return res.send({
+                                success: false,
+                                message: "Invalid credential(s)"
+                        });
+                console.log(adminUser);
         } catch (e) {
-                res.send({ error: `${e}` });
+                res.status(500).send({
+                        success: false,
+                        message: "Internal server error: " + e
+                });
         }
 });
 
-app.post('/api/v1/franchisor/register', (req, res) => {
-        
-});
-
-// user login
-app.post('/api/v1/investor/login', (req, res) => {
-        
-});
-
-app.post('/api/v1/franchisor/login', (req, res) => {
-        
-});
-
-app.listen(process.env.PORT || 4000, () => console.log(`Listening on PORT ${process.env.PORT}`));
+app.listen(process.env.PORT || 3000, () => console.log(`Listening on PORT ${process.env.PORT}...`));
