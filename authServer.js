da08d7c20560ad76b266b1e0e50cbf37674fa3f5
jay-io <jay.dnb@outlook.in>
2022-05-30 11:35:21 +0530

    Remove mongoose module import

--- authServer.js
+++ authServer.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const jwt  = require('jsonwebtoken');
 const bcrypt  = require('bcrypt');
-const mongoose = require('mongoose');
 
 const admcred = require('./schemas/admin-login-cred');
 const admtoken = require('./schemas/admin-access-token');
