2cce43a065e29a9f0f70cf478edb41f35859ce94
jay-io <jay.dnb@outlook.in>
2022-05-31 07:16:54 +0530

    Add schema for trending-videos collection

---
+++ schemas/trending-videos.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const trendingVideosSchema = new mongoose.Schema({
+        thumbnail_link: {
+                type: String,
+                required: true
+        },
+        title: {
+                type: String,
+                required: true
+        },
+        description: {
+                type: String,
+                required: true
+        }
+}, {
+        versionKey: false
+});
+
+module.exports = mongoose.model('trendingVideos', trendingVideosSchema, 'trending-videos');
