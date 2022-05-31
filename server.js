afa4df08b0eca6cf0ea0dee3f20e1f864074d195
jay-io <jay.dnb@outlook.in>
2022-05-31 07:13:35 +0530

    Add admin logout route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bcrypt  = require('bcrypt');
 const admcred = require('./schemas/admin-login-cred');
 const admtoken = require('./schemas/admin-access-token');
 const sweeptoken = require('./cronjobs/tokenSweep');
+const dashboard = require('./adminDashboard');
 
 require('dotenv').config();
 
@@ -15,6 +16,7 @@ sweeptoken.start();
 
 // init. middleware
 const isAdmin = require('./middlewares/isAdmin');
+app.use('/api/v1/admin/dashboard', dashboard);
 app.use(express.json());
 
 // admin login
@@ -62,6 +64,30 @@ app.post('/api/v1/admin/login', isAdmin, async (req, res) => {
         }
 });
 
+app.post('api/v1/admin/logout', isAdmin, async (req, res) => {
+        try {
+                if (req.IS_TOKEN_VALID) {
+                        await admtoken.deleteOne({
+                                access_token: req.headers.authorization.split(' ')[1]
+                        });
+                        return res.json({
+                                success: true,
+                                message: "Session Expired: You have been logged out of administrative panel"
+                        });
+                }
+                else
+                        return res.json({
+                                success: false,
+                                message: "Invalid Credential(s): You don't have administrative access to log out of"
+                        });
+        } catch (e) {
+                res.status(500).json({
+                        success: false,
+                        message: "Internal server error: " + e
+                });
+        }
+});
+ 
 app.listen(
         process.env.PORT || 3000,
         () => console.log(`Listening on PORT ${process.env.PORT}...`)
