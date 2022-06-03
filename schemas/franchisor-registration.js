d35401a4e00f3671bb787be4618c61cae1ff053c
jay-io <jay.dnb@outlook.in>
2022-06-03 19:28:06 +0530

    Remove verbose validation checks; add more fields

--- schemas/franchisor-registration.js
+++ schemas/franchisor-registration.js
@@ -19,138 +19,67 @@ const fregnSchema = new mongoose.Schema({
                         type: Number,
                         required: true,
                         default: 0
-                },
-                category: {
-                        type: String,
-                        enum: [
-                                "automotive",
-                                "beauty-and-health",
-                                "business-services",
-                                "dealers-and-distributors",
-                                "education",
-                                "fashion",
-                                "food-and-beverage",
-                                "home-based-business",
-                                "hotel-travel-and-tourism",
-                                "retail",
-                                "sports-fitness-and-entertainment",
-                                "others"
-                        ],
-                        default: "others"
                 }
         },
-        email: {
-                type: String,
-                required: true,
-        },
-        phone: {
-                type: String,
-                required: true
-        },
         content: {
-                business_details: {
-                        type: String,
-                        required: true,
-                        minlength: 500,
-                        maxlength: 4096
-                },
-                investment_details: {
-                        op_comm_on: {
-                                type: Number,
-                                required: true,
-                                min: 1800,
-                                max: () => new Date().getFullYear()
+                personal_details: {
+                        email_id: { required: true },
+                        mobile: { required: true },
+                        brand_name: { required: true },
+                        company_name: { required: true },
+                        owner_name: { required: true },
+                        owner_email: { required: true },
+                        owner_phone: { required: true },
+                        address_det: { required: true },
+                        country: { required: true },
+                        state: { required: true },
+                        city: { required: true },
+                        pincode: { required: true },
+                        industry: { required: true },
+                        no_of_franch_outlets: { required: true },
+                        inv: { required: true },
+                        business_desc: { required: true },
+                        q_excl_terr_rights: { required: true },
+                        q_perf_guarantees: { required: true },
+                        q_adv_market_levies: { required: true },
+                        antc_perc_ret: { required: true },
+                        likely_payback_period: {
+                                min: { required: true },
+                                max: { required: true },
+                                month_yr: { required: true },
                         },
-                        fran_comm_on: {
-                                type: Number,
-                                required: true,
-                                min: 1800,
-                                max: () => new Date().getFullYear()
-                        },
-                        investment: {
-                                type: String,
-                                required: true
-                        },
-                        fran_brand_fee: {
-                                type: String,
-                                required: true
-                        },
-                        royalty_commssn: {
-                                type: Number,
-                                required: true,
-                                min: 0,
-                                max: 100
-                        },
-                        excl_terr_rights: {
-                                type: Boolean,
-                                required: true
-                        },
-                        antc_pcent_return: {
-                                type: Number,
-                                required: true,
-                                min: 0,
-                                max: 100
-                        },
-                        pay_back_period: {
-                                type: String,
-                                required: true
-                        },
-                        other_inv_req: {
-                                type: String,
-                                required: true,
-                                default: "None"
-                        }
+                        other_inv_req: { required: false },
                 },
                 property_details: {
-                        prop_type: {
-                                type: String,
-                                required: true
+                        typ_prop_req: { required: true },
+                        floor_area_req: {
+                                min: { required: true },
+                                max: { required: true }
                         },
-                        flor_area_req: {
-                                type: String,
-                                required: true
-                        },
-                        pref_loc: {
-                                type: String,
-                                required: true
-                        }
+                        pref_loc: { required: true },
+                        q_site_select_assist: { required: true },
                 },
                 training_details: {
-                        det_op_manual: {
-                                type: Boolean,
-                                required: true
-                        },
-                        fran_training_loc: {
-                                type: String,
-                                required: true
-                        },
-                        field_assist_avail: {
-                                type: Boolean,
-                                required: true
-                        },
-                        expert_guid: {
-                                type: Boolean,
-                                required: true
-                        },
-                        curr_itsys_incl: {
-                                type: Boolean,
-                                required: true
-                        }
+                        q_hv_det_op_man: { required: true },
+                        whr_franch_training: { required: true },
+                        q_field_assist_avail: { required: true }
                 },
-                agreement_term_details: {
-                        how_long: {
-                                type: String,
-                                required: true
-                        },
-                        is_term_renew: {
-                                type: Boolean,
-                                required: true
-                        }
+                agreement_details: {
+                        q_hv_std_franch_agr: { required: true },
+                        duration_of_contr: { required: true },
+                        q_term_renewable: { required: true }
+                },
+                final_rites: {
+                        company_logo: { required: true },
+                        video_link: { required: false },
+                        gst_no: { required: true },
+                        mode_of_payment: { required: true },
+                        q_sub_news: { required: true }
                 }
         }
 }, {
         versionKey: false,
-        timestamps: true
+        timestamps: true,
 });
 
 module.exports = mongoose.model('fregn', fregnSchema, 'franchisor-registration');
