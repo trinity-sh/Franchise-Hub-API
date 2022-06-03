136486651233e4c3f2e01523043b1a144c59a261
jay-io <jay.dnb@outlook.in>
2022-06-03 19:32:54 +0530

    Add route handler for `Upcoming Events` website section

---
+++ routes/webview/section-events.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+// init. schemas
+const fregn = require('../../schemas/franchisor-registration');
+const news = require('../../schemas/newsletter-subscribers');
+const admcred = require('../../schemas/admin-login-cred');
+const tvid = require('../../schemas/trending-videos');
+const fadv = require('../../schemas/free-advice');
+const events = require('../../schemas/events');
+
+// init. middlewares
+router.use(express.json());
+
+router.get('/all', async (req, res) => {
+        try {
+                const edocs = await events.find({  });
+                
+                return res.json({
+                        success: true,
+                        payload: edocs
+                });
+        } catch (e) {
+                res.status(500).json({
+                        success: false,
+                        message: "Internal server error: " + e
+                });
+        }
+});
+
+module.exports = router;
