375d5e353e3ae8fd24cd59d3eeeaefc3ae0e9a56
jay-io <jay.dnb@outlook.in>
2022-06-03 19:31:21 +0530

    Add route handlers for `Top Franchise Opportunities` website section

---
+++ routes/webview/section-top-franc-opp.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+// init. schemas
+const fregn = require('../../schemas/franchisor-registration');
+const news = require('../../schemas/newsletter-subscribers');
+const admcred = require('../../schemas/admin-login-cred');
+const tvid = require('../../schemas/trending-videos');
+const fadv = require('../../schemas/free-advice');
+const events = require('../../schemas/events');
+
+// init. middlewares
+router.use(express.json());
+
+router.get('/all', async (req, res) => {
+        try {
+                const fregndoc = await fregn.find({
+                        "metadata.is_listed": true,
+                        "metadata.is_promoted": true
+                });
+                
+                return res.json({
+                        success: true,
+                        payload:  fregndoc
+                });
+        } catch (e) {
+                res.status(500).json({
+                        success: false,
+                        message: "Internal server error: " + e
+                });
+        }
+});
+
+module.exports = router;
