a39365237db1bd5ac628ba31fdc00897229cb929
jay-io <jay.dnb@outlook.in>
2022-06-03 19:25:04 +0530

    Restructure dictionary hierarchy

--- schemas/newsletter-subscribers.js
+++ schemas/newsletter-subscribers.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose')
 
 const newsSubsSchema = new mongoose.Schema({
-        is_read: {
-                type: Boolean,
-                required: true,
-                default: false
+        metadata: {
+                is_read: {
+                        type: Boolean,
+                        required: true,
+                        default: false
+                }
         },
-        email: {
-                type: String,
-                required: true
+        content: {
+                email: {
+                        type: String,
+                        required: true
+                }
         }
 }, {
         versionKey: false,
