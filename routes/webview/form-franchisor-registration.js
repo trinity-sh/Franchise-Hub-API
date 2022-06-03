100e4e4893a8dcc397a00e6e17e0dc8be1952eea
jay-io <jay.dnb@outlook.in>
2022-06-03 19:36:05 +0530

    Add route handler for `Expand Your Franchise` website form

---
+++ routes/webview/form-franchisor-registration.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+// init. schemas
+const fregn = require('../../schemas/franchisor-registration');
+const news = require('../../schemas/newsletter-subscribers');
+const admcred = require('../../schemas/admin-login-cred');
+const tvid = require('../../schemas/trending-videos');
+const fadv = require('../../schemas/free-advice');
+const events = require('../../schemas/events');
+
+// init. middlewares
+router.use(express.json());
+
+router.post('/new', async (req, res) => {
+        try {
+                const fregndoc = new fregn(req.body);
+                try {
+                        await fregndoc.save();
+                } catch (e) {
+                        return res.json({
+                                success: false,
+                                message: "Form error: " + e
+                        });
+                }
+                
+                return res.json({
+                        success: true,
+                        message: "Video added successfully"
+                });
+        } catch (e) {
+                res.status(500).json({
+                        success: false,
+                        message: "Internal server error: " + e
+                });
+        }
+});
+
+module.exports = router;
