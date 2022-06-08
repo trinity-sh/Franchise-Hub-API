99d391d4a8a77c449835b93620045c19bfd582b3
jay-io <jay.dnb@outlook.in>
2022-06-08 18:16:11 +0530

    Add inclusion for franchisee-application handler

--- routes/admin-dashboard/dashb-forms.js
+++ routes/admin-dashboard/dashb-forms.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // init. router
 router.use('/franchisor-registration', require('./form-franchisor-registration'));
 router.use('/newsletter-subscribers', require('./form-newsletter-subscribers'));
+router.use('/franchisee-application', require('./form-apply-for-franchisee'));
 router.use('/free-advice', require('./form-free-advice'));
 
 module.exports = router;
