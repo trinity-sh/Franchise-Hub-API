f3cc4b4b80b13aeed7b396166740dcaa39176170
jay-io <jay.dnb@outlook.in>
2022-06-08 19:50:07 +0530

    Add handler for franchisee-application route

--- routes/webView.js
+++ routes/webView.js
@@ -5,10 +5,11 @@ const router = express.Router();
 router.use('/forms/franchisor-registration', require('./webview/form-franchisor-registration'));
 router.use('/section/top-franchise-opportunities', require('./webview/section-top-franc-opp'));
 router.use('/forms/newsletter-subscribers', require('./webview/form-newsletter-subscribers'));
+router.use('form/franchisee-application', require('./webview/form-apply-for-franchisee'));
 router.use('/section/listed-franchises', require('./webview/section-listed-franchises'));
 router.use('/section/trending-videos', require('./webview/section-trending-videos'));
-router.use('/section/events', require('./webview/section-events'));
 router.use('/forms/free-advice', require('./webview/form-free-advice'));
 router.use('/section/filter', require('./webview/section-filter'));
+router.use('/section/events', require('./webview/section-events'));
 
 module.exports = router;
