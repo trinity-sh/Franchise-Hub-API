f5c0426c66740516a37976f699f92cec535e634a
jay-io <jay.dnb@outlook.in>
2022-06-08 22:26:50 +0530

    Restructure schema

--- schemas/events.js
+++ schemas/events.js
@@ -1,22 +1,20 @@
 const mongoose = require('mongoose')
 
-const eventSchema = new mongoose.Schema({
-        content: {        
-                event_banner: {
-                        type: String
-                },
-                event_title: {
-                        type: String
-                },
-                event_link: {
-                        type: String
-                },
-                event_date: {
-                        type: String
-                },
-                hotline_no: {
-                        type: String
-                }
+const eventSchema = new mongoose.Schema({     
+        event_banner: {
+                type: String
+        },
+        event_title: {
+                type: String
+        },
+        event_link: {
+                type: String
+        },
+        event_date: {
+                type: String
+        },
+        hotline_no: {
+                type: String
         }
 }, {
         versionKey: false,
