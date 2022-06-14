105a63ad2857e1cf3a1ced4291a7bfbf51fe1067
Jayanta Pandit <jay.dnb@outlook.in>
2022-06-14 12:15:50 +0530

    Use std route URI for franchisee-application

--- routes/webView.js
+++ routes/webView.js
@@ -6,7 +6,7 @@ router.use('/forms/franchisor-registration', require('./webview/form-franchisor-
 router.use('/section/top-franchise-opportunities', require('./webview/section-top-franc-opp'));
 router.use('/forms/newsletter-subscribers', require('./webview/form-newsletter-subscribers'));
 router.use('/section/trending-franchises', require('./webview/section-trending-franchises'));
-router.use('form/franchisee-application', require('./webview/form-apply-for-franchisee'));
+router.use('forms/franchisee-application', require('./webview/form-apply-for-franchisee'));
 router.use('/section/listed-franchises', require('./webview/section-listed-franchises'));
 router.use('/section/trending-videos', require('./webview/section-trending-videos'));
 router.use('/forms/free-advice', require('./webview/form-free-advice'));
